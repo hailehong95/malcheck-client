03d8a006af3791048996d398f0c0f1efe495106e
Hai Le Hong <20485451+hailehong95@users.noreply.github.com>
2023-02-01 11:20:51 +0700

    Update: fix logging to file

--- malcheck_client/logging.py
+++ malcheck_client/logging.py
@@ -2,8 +2,9 @@ import logging
 from malcheck_client.config import LOG_PATH
 
 logger = logging.getLogger()
-logFormatter = logging.Formatter('%(asctime)s - Malcheck-Client - %(levelname)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
-fileHandler = logging.FileHandler(LOG_PATH)
-fileHandler.setLevel(logging.INFO)
-fileHandler.setFormatter(logFormatter)
-logger.addHandler(fileHandler)
+logger.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s - Malcheck-Client - %(levelname)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+file_handler = logging.FileHandler(LOG_PATH)
+file_handler.setLevel(logging.DEBUG)
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
